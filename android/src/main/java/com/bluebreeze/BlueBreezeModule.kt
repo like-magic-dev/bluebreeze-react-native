d49bbff7b2402a5a8fc8a4b4b2b03c076cd9f8bc
Alessandro Mulloni <ale@mulloni.tech>
2025-02-05 23:08:10 +0100

    Implement first methods

--- android/src/main/java/com/bluebreeze/BlueBreezeModule.kt
+++ android/src/main/java/com/bluebreeze/BlueBreezeModule.kt
@@ -4,127 +4,341 @@ import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.WritableArray
+import com.facebook.react.bridge.WritableNativeArray
 import com.facebook.react.module.annotations.ReactModule
+import dev.likemagic.bluebreeze.BBAuthorization
+import dev.likemagic.bluebreeze.BBManager
+import dev.likemagic.bluebreeze.BBState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 @ReactModule(name = BlueBreezeModule.NAME)
-class BlueBreezeModule(reactContext: ReactApplicationContext) :
-  NativeBlueBreezeSpec(reactContext) {
+class BlueBreezeModule(reactContext: ReactApplicationContext) : NativeBlueBreezeSpec(reactContext) {
+    val manager = BBManager(reactContext)
+    val jobs: MutableList<Job> = mutableListOf()
 
-  override fun getName(): String {
-    return NAME
-  }
+//    NSMutableArray<NSString*>* trackedDeviceServices;
+//    NSMutableArray<NSString*>* trackedDeviceConnectionStatus;
+//    NSMutableArray<NSString*>* trackedDeviceMTU;
+//    NSMutableArray<NSString*>* trackedDeviceCharacteristicData;
+//    NSMutableArray<NSString*>* trackedDeviceCharacteristicNotifyEnabled;
 
-  override fun state(): String {
-    TODO("Not yet implemented")
-  }
+    init {
+        jobs.launch {
+            manager.state.collect {
+                emitStateEmitter(it.name)
+            }
+        }
 
-  override fun authorizationStatus(): String {
-    TODO("Not yet implemented")
-  }
+        jobs.launch {
+            manager.authorizationStatus.collect {
+                emitAuthorizationStatusEmitter(it.export)
+            }
+        }
 
-  override fun authorizationRequest() {
-    TODO("Not yet implemented")
-  }
+        jobs.launch {
+            manager.scanEnabled.collect {
+                emitScanningEnabledEmitter(it)
+            }
+        }
 
-  override fun scanningEnabled(): Boolean {
-    TODO("Not yet implemented")
-  }
+//        [impl devicesObserveOnChanged:^(NSArray<id<NSObject>> *devices) {
+//            for (id device in devices) {
+//            NSString* deviceId = device[@"id"];
+//
+//            if ([self->trackedDeviceServices indexOfObject:deviceId] == NSNotFound) {
+//            [self->trackedDeviceServices addObject:deviceId];
+//            [self->impl deviceServicesObserveWithId:deviceId onChanged:^(NSArray<id<NSObject>> * _Nonnull value) {
+//            [self emitDeviceServicesEmitter:@{
+//                @"id": deviceId,
+//                @"value": value
+//            }];
+//
+//            for (id service in value) {
+//            NSString* serviceId = service[@"id"];
+//            for (id characteristic in service[@"characteristics"]) {
+//            NSString* characteristicId = characteristic[@"id"];
+//            NSString* trackingKey = [NSString stringWithFormat:@"%@:%@:%@", deviceId, serviceId, characteristicId];
+//
+//            if ([self->trackedDeviceCharacteristicData indexOfObject:trackingKey] == NSNotFound) {
+//            [self->trackedDeviceConnectionStatus addObject:trackingKey];
+//            [self->impl deviceCharacteristicDataObserveWithId:deviceId serviceId:serviceId characteristicId:characteristicId onChanged:^(NSArray<NSNumber *> * _Nonnull value) {
+//            [self emitDeviceCharacteristicDataEmitter:@{
+//                @"id": deviceId,
+//                @"serviceId": serviceId,
+//                @"characteristicId": characteristicId,
+//                @"value": value
+//            }];
+//        }];
+//        }
+//
+//            if ([self->trackedDeviceCharacteristicNotifyEnabled indexOfObject:trackingKey] == NSNotFound) {
+//            [self->trackedDeviceCharacteristicNotifyEnabled addObject:trackingKey];
+//            [self->impl deviceCharacteristicNotifyEnabledObserveWithId:deviceId serviceId:serviceId characteristicId:characteristicId onChanged:^(BOOL value) {
+//            [self emitDeviceCharacteristicNotifyEnabledEmitter:@{
+//                @"id": deviceId,
+//                @"serviceId": serviceId,
+//                @"characteristicId": characteristicId,
+//                @"value": [NSNumber numberWithBool:value]
+//            }];
+//        }];
+//        }
+//        }
+//        }
+//        }];
+//        }
+//
+//            if ([self->trackedDeviceConnectionStatus indexOfObject:deviceId] == NSNotFound) {
+//            [self->trackedDeviceConnectionStatus addObject:deviceId];
+//            [self->impl deviceConnectionStatusObserveWithId:deviceId onChanged:^(NSString * _Nonnull value) {
+//            [self emitDeviceConnectionStatusEmitter:@{
+//                @"id": deviceId,
+//                @"value": value
+//            }];
+//        }];
+//        }
+//
+//            if ([self->trackedDeviceMTU indexOfObject:deviceId] == NSNotFound) {
+//            [self->trackedDeviceMTU addObject:deviceId];
+//            [self->impl deviceMTUObserveWithId:deviceId onChanged:^(NSInteger value) {
+//            [self emitDeviceMTUEmitter:@{
+//                @"id": deviceId,
+//                @"value": [NSNumber numberWithInt:value]
+//            }];
+//        }];
+//        }
+//        }
+//
+//            if (self->_eventEmitterCallback != nil) {
+//            [self emitDevicesEmitter:devices];
+//        }
+//        }];
+    }
 
-  override fun scanningStart() {
-    TODO("Not yet implemented")
-  }
+    override fun invalidate() {
+        super.invalidate()
 
-  override fun scanningStop() {
-    TODO("Not yet implemented")
-  }
+        jobs.forEach { it.cancel() }
+        jobs.clear()
+    }
 
-  override fun devices(): WritableArray {
-    TODO("Not yet implemented")
-  }
+    override fun getName(): String {
+        return NAME
+    }
 
-  override fun deviceServices(id: String?): WritableArray {
-    TODO("Not yet implemented")
-  }
+    override fun state(): String = manager.state.value.name
 
-  override fun deviceConnectionStatus(id: String?): String {
-    TODO("Not yet implemented")
-  }
+    override fun authorizationStatus(): String = manager.authorizationStatus.value.export
 
-  override fun deviceMTU(id: String?): Double {
-    TODO("Not yet implemented")
-  }
+    override fun authorizationRequest() = manager.authorizationRequest(reactApplicationContext)
 
-  override fun deviceConnect(id: String?, promise: Promise?) {
-    TODO("Not yet implemented")
-  }
+    override fun scanningEnabled(): Boolean = manager.scanEnabled.value
 
-  override fun deviceDisconnect(id: String?, promise: Promise?) {
-    TODO("Not yet implemented")
-  }
+    override fun scanningStart() = manager.scanStart(reactApplicationContext)
 
-  override fun deviceDiscoverServices(id: String?, promise: Promise?) {
-    TODO("Not yet implemented")
-  }
+    override fun scanningStop() = manager.scanStop(reactApplicationContext)
 
-  override fun deviceRequestMTU(id: String?, mtu: Double, promise: Promise?) {
-    TODO("Not yet implemented")
-  }
+    override fun devices(): WritableArray {
+        return WritableNativeArray()
+    }
 
-  override fun deviceCharacteristicData(
-    id: String?,
-    serviceId: String?,
-    characteristicId: String?
-  ): WritableArray {
-    TODO("Not yet implemented")
-  }
+    override fun deviceServices(id: String?): WritableArray {
+        return WritableNativeArray()
+    }
 
-  override fun deviceCharacteristicNotifyEnabled(
-    id: String?,
-    serviceId: String?,
-    characteristicId: String?
-  ): Boolean {
-    TODO("Not yet implemented")
-  }
+    override fun deviceConnectionStatus(id: String?): String {
+        return ""
+    }
 
-  override fun deviceCharacteristicRead(
-    id: String?,
-    serviceId: String?,
-    characteristicId: String?,
-    promise: Promise?
-  ) {
-    TODO("Not yet implemented")
-  }
+    override fun deviceMTU(id: String?): Double {
+        return 0.0
+    }
 
-  override fun deviceCharacteristicWrite(
-    id: String?,
-    serviceId: String?,
-    characteristicId: String?,
-    data: ReadableArray?,
-    withResponse: Boolean,
-    promise: Promise?
-  ) {
-    TODO("Not yet implemented")
-  }
+    override fun deviceConnect(id: String?, promise: Promise?) {
+    }
 
-  override fun deviceCharacteristicSubscribe(
-    id: String?,
-    serviceId: String?,
-    characteristicId: String?,
-    promise: Promise?
-  ) {
-    TODO("Not yet implemented")
-  }
+    override fun deviceDisconnect(id: String?, promise: Promise?) {
+    }
 
-  override fun deviceCharacteristicUnsubscribe(
-    id: String?,
-    serviceId: String?,
-    characteristicId: String?,
-    promise: Promise?
-  ) {
-    TODO("Not yet implemented")
-  }
+    override fun deviceDiscoverServices(id: String?, promise: Promise?) {
+    }
 
-  companion object {
-    const val NAME = "BlueBreeze"
-  }
+    override fun deviceRequestMTU(id: String?, mtu: Double, promise: Promise?) {
+    }
+
+    override fun deviceCharacteristicData(
+        id: String?,
+        serviceId: String?,
+        characteristicId: String?
+    ): WritableArray {
+        return WritableNativeArray()
+    }
+
+    override fun deviceCharacteristicNotifyEnabled(
+        id: String?,
+        serviceId: String?,
+        characteristicId: String?
+    ): Boolean {
+        return false
+    }
+
+    override fun deviceCharacteristicRead(
+        id: String?,
+        serviceId: String?,
+        characteristicId: String?,
+        promise: Promise?
+    ) {
+    }
+
+    override fun deviceCharacteristicWrite(
+        id: String?,
+        serviceId: String?,
+        characteristicId: String?,
+        data: ReadableArray?,
+        withResponse: Boolean,
+        promise: Promise?
+    ) {
+    }
+
+    override fun deviceCharacteristicSubscribe(
+        id: String?,
+        serviceId: String?,
+        characteristicId: String?,
+        promise: Promise?
+    ) {
+    }
+
+    override fun deviceCharacteristicUnsubscribe(
+        id: String?,
+        serviceId: String?,
+        characteristicId: String?,
+        promise: Promise?
+    ) {
+    }
+
+    companion object {
+        const val NAME = "BlueBreeze"
+    }
 }
+
+/// Export native values
+
+val BBAuthorization.export: String
+    get() = when(this) {
+        BBAuthorization.unknown -> "unknown"
+        BBAuthorization.showRationale -> "showRationale"
+        BBAuthorization.authorized -> "authorized"
+        BBAuthorization.denied -> "denied"
+    }
+
+val BBState.export: String
+    get() = when(this) {
+        BBState.unknown -> "unknown"
+        BBState.poweredOff -> "poweredOff"
+        BBState.poweredOn -> "poweredOn"
+        BBState.unauthorized -> "unauthorized"
+    }
+
+//extension BBCharacteristicProperty {
+//    var export: String {
+//        switch self {
+//            case .read: return "read"
+//            case .writeWithoutResponse: return "writeWithoutResponse"
+//            case .writeWithResponse: return "writeWithResponse"
+//            case .notify: return "notify"
+//        }
+//    }
+//}
+//
+//extension BBDevice {
+//    var export: Dictionary<String, Any> {
+//        var result: Dictionary<String, Any> = [
+//            "id": id.uuidString,
+//        "rssi": rssi,
+//        "isConnectable": isConnectable,
+//        "advertisedServices": advertisedServices.map(\.uuidString),
+//        ]
+//
+//        if let name {
+//            result["name"] = name
+//        }
+//
+//        if let manufacturerId {
+//            result["manufacturerId"] = manufacturerId
+//        }
+//
+//        if let manufacturerName {
+//            result["manufacturerName"] = manufacturerName
+//        }
+//
+//        if let manufacturerData {
+//            result["manufacturerData"] = manufacturerData.export
+//        }
+//
+//        return result;
+//    }
+//
+//    func getCharacteristic(serviceId: String, characteristicId: String) -> BBCharacteristic? {
+//        let service = services.value.first { $0.key == BBUUID(string: serviceId)}
+//        let characteristic = service?.value.first { $0.id == BBUUID(string: characteristicId) }
+//        return characteristic
+//    }
+//}
+//
+//extension BBDeviceConnectionStatus {
+//    var export: String {
+//        switch self {
+//            case .connected: return "connected"
+//            case .disconnected: return "disconnected"
+//        }
+//    }
+//}
+//
+//extension BBCharacteristic {
+//    var export: Dictionary<String, Any> {
+//        var result: Dictionary<String, Any> = [
+//            "id": id.uuidString,
+//        "properties": properties.map { $0.export }
+//        ]
+//
+//        if let name = BBConstants.knownCharacteristics[id] {
+//            result["name"] = name
+//        }
+//
+//        return result
+//    }
+//}
+//
+//extension [BBUUID: [BBCharacteristic]] {
+//    var export: [Dictionary<String, Any>] {
+//        return map { element in
+//            var result: Dictionary<String, Any> = [
+//                "id": element.key.uuidString,
+//            "characteristics": element.value.map { $0.export }
+//            ]
+//
+//            if let name = BBConstants.knownServices[element.key] {
+//                result["name"] = name
+//            }
+//
+//            return result
+//        }
+//    }
+//}
+//
+//extension Data {
+//    var export: Array<UInt8> {
+//        return map { $0 }
+//    }
+//}
+
+/// Functions to store jobs
+
+fun Job.storeIn(list: MutableList<Job>) = list.add(this)
+
+fun MutableList<Job>.launch(block: suspend CoroutineScope.() -> Unit) =
+    CoroutineScope(Dispatchers.IO)
+        .launch(block = block)
+        .storeIn(this)
